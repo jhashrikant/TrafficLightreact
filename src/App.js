e123a8d73c3ba8004920f5d03e8b149e806c847d
Shrikant Jha <87967393+jhashrikant@users.noreply.github.com>
2024-04-23 14:11:08 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+const trafficLights = {
+  red: {
+    color: 'red',
+    bgColor: 'red',
+    waitTime: 4000,
+    nextColor: 'green'
+  },
+  yellow: {
+    color: 'yellow',
+    bgColor: 'yellow',
+    waitTime: 500,
+    nextColor: 'red'
+  },
+  green: {
+    color: 'green',
+    bgColor: 'green',
+    waitTime: 3000,
+    nextColor: 'yellow'
+  }
+}
+
 function App() {
+
+  const [currentColor, setcurrentColor] = useState('red')
+
+  useEffect(() => {
+    console.log(currentColor)
+    const timer = setTimeout(() => {
+      setcurrentColor(trafficLights[currentColor]?.nextColor)
+    }, trafficLights[currentColor]?.waitTime);
+    //cleanup
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [currentColor])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main__Container'>
+      <div className='grid'>
+        {Object.keys(trafficLights).map((color) => {
+          return (
+            <div key={color} className='trafficLight' style={{ backgroundColor: color === currentColor ? trafficLights[color].bgColor : undefined }}></div>
+          )
+        })}
+      </div>
     </div>
   );
 }
